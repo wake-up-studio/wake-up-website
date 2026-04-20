2f08f6cdd18ae5bde81b2a7fe9f792b80d0f9b39
wake-up-studio <wakeuphuis@gmail.com>
2026-04-20 11:01:12 +0200

    crud admin done

--- index.php
+++ index.php
@@ -4,5 +4,10 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 
 require "config/autoload.php";
 
+require __DIR__ . '/vendor/autoload.php';
+// Chargement des variables d'environnement
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $router = new Router();
 $router -> handleRequest($_GET);
--- managers/AbstractManager.php
+++ managers/AbstractManager.php
@@ -4,13 +4,13 @@ abstract class AbstractManager{
 
     public function __construct()
     {
-        $host = "db.3wa.io";
-        $port = "3306";
-        $dbname = "gurvanleroy_wake_up";
+        $host = $_ENV['DB_HOST'];
+        $port = $_ENV["DB_PORT"];
+        $dbname = $_ENV["DB_NAME"];
         $connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
 
-        $user = "gurvanleroy";
-        $password = "2d829f3ed3cc9932b6d2f2c9c09fe64c";
+        $user = $_ENV["DB_USER"];
+        $password = $_ENV["DB_PASS"];
 
         $this->db = new PDO(
             $connexionString,
